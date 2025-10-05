533eda96a6a99b5f5cb971010ad27dcc9644c78e
HerodVelasco <2201651@slu.edu.ph>
2025-10-05 16:05:01 +0800

    Added and defined mongoose express

--- routes/trainers.js
+++ routes/trainers.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const asyncHandler = require('../middleware/asyncHandler');
 const Trainer = require('../models/Trainer');
 const Feedback = require('../models/Feedback');
